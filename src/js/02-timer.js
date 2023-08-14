9c04a874902644efe37c94698183624dbcdd0ae3
LiudmylaHorobets <horobets1323@gmail.com>
2023-08-14 22:31:32 +0300

    task 3

---
+++ src/js/02-timer.js
@@ -0,0 +1,14 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import { Report } from 'notiflix/build/notiflix-report-aio';
+
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
