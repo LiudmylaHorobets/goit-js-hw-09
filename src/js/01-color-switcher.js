836a4614b499970a8c3a60574104d9f11cbe98db
LiudmylaHorobets <horobets1323@gmail.com>
2023-08-14 21:59:21 +0300

    finished task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startButton = document.getElementById('startButton');
+const stopButton = document.getElementById('stopButton');
+let idInterval;
 
+startButton.addEventListener('click', () => {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+  idInterval = setInterval(changeBackgroundColor, 1000);
+});
+
+stopButton.addEventListener('click', () => {
+  clearInterval(idInterval);
+  startButton.disabled = false;
+  stopButton.disabled = true;
+});
+
+function changeBackgroundColor() {
+  const randomColor = getRandomColor();
+  document.body.style.backgroundColor = randomColor;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
