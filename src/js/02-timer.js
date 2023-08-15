6ae28f6cc1c8004c3587e752ae31d08ed888427d
LiudmylaHorobets <horobets1323@gmail.com>
2023-08-15 12:36:49 +0300

    finished task 3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,14 +1,78 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
-import { Report } from 'notiflix/build/notiflix-report-aio';
+import Notiflix from 'notiflix';
 
+const datetimePicker = document.getElementById('datetime-picker');
+const startButton = document.querySelector('[data-start]');
+const daysSpan = document.querySelector('[data-days]');
+const hoursSpan = document.querySelector('[data-hours]');
+const minutesSpan = document.querySelector('[data-minutes]');
+const secondsSpan = document.querySelector('[data-seconds]');
 
-const options = {
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function updateTimerDisplay(days, hours, minutes, seconds) {
+  daysSpan.textContent = addLeadingZero(days);
+  hoursSpan.textContent = addLeadingZero(hours);
+  minutesSpan.textContent = addLeadingZero(minutes);
+  secondsSpan.textContent = addLeadingZero(seconds);
+}
+
+function startTimer(targetDate) {
+  const intervalId = setInterval(() => {
+    const currentTime = new Date().getTime();
+    const timeLeft = targetDate - currentTime;
+
+    if (timeLeft < 0) {
+      clearInterval(intervalId);
+      updateTimerDisplay(0, 0, 0, 0);
+      Notiflix.Notify.failure('Time is up!');
+    } else {
+      const { days, hours, minutes, seconds } = convertMs(timeLeft);
+      updateTimerDisplay(days, hours, minutes, seconds);
+    }
+  }, 1000);
+}
+
+flatpickr('#datetime-picker', {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    const selectedDate = selectedDates[0];
+    const currentDate = new Date();
+
+    if (selectedDate <= currentDate) {
+      Notiflix.Notify.warning('Please choose a date in the future');
+      startButton.disabled = true;
+    } else {
+      startButton.disabled = false;
+      const { days, hours, minutes, seconds } = convertMs(
+        selectedDate - currentDate
+      );
+      updateTimerDisplay(days, hours, minutes, seconds);
+    }
   },
-};
+});
+
+startButton.addEventListener('click', () => {
+  const selectedDate = new Date(datetimePicker.value);
+  startTimer(selectedDate);
+});
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
